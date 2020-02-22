d5e15baf3f4e535b8ea6cf69c018bb20cffee2ec
PraxLannister <prakharsp1502077@gmail.com>
2020-02-22 14:49:47 +0530

    Initial Commit

---
+++ gitProxy.sh
@@ -0,0 +1,2 @@
+git config --global http.proxy http://edcguest:edcguest@172.31.100.29:3128
+git config --global https.proxy http://edcguest:edcguest@172.31.100.29:3128
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
+
+
